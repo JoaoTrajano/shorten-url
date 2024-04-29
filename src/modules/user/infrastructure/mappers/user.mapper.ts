328a5c8c2c23d787f7f9ecbd340228fad8af5ea3
joao-trajano <joao.trajanosouza@gmail.com>
2024-04-29 20:41:49 -0300

    fix: user mappers

--- src/modules/user/infrastructure/mappers/user.mapper.ts
+++ src/modules/user/infrastructure/mappers/user.mapper.ts
@@ -1,6 +1,7 @@
 import { Prisma } from '@prisma/client';
 import { UserEntity } from '../../domain/entities/user.entity';
 import { Password } from '../../value-objects/password.value-object';
+import { User as UserSchema } from '@prisma/client';
 
 export class UserMapper {
   static toDomain(schema: Prisma.UserGetPayload<Prisma.UserArgs>): UserEntity {
@@ -18,23 +19,14 @@ export class UserMapper {
     return entity;
   }
 
-  // static toPersistence(user: User): UserSchema {
-  //   return {
-  //     id: user.id,
-  //     name: user.name,
-  //     email: user.email?.value ?? null,
-  //     active: user.active,
-  //     inactivedAt: user.inactivedAt,
-  //     type: user.type as UserTypeSchema,
-  //     password: user.password.value,
-  //     permissions: user.permissions as unknown as Prisma.JsonValue,
-  //     document: user.document?.value ?? null,
-  //     documentType: user.document?.type ?? null,
-  //     createdAt: user.createdAt,
-  //     updatedAt: user.updatedAt,
-  //     lastLoginAt: user.lastLoginAt,
-  //     totalAttemptsSendSms: user.totalAttemptsSendSms,
-  //     dateBlockAttemptsSendSms: user.dateBlockAttemptsSendSms,
-  //   };
-  // }
+  static toPersistence(user: UserEntity): UserSchema {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
+  }
 }
