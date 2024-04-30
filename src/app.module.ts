9e5622c39f663042abd35ed5ca34cb7a16a5e0b0
joao-trajano <joao.trajanosouza@gmail.com>
2024-04-30 03:03:21 -0300

    feat: env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { ShortUrlModule } from './modules/url/short-url.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
